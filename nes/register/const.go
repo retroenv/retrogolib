5e73621183446cbfa4794ebbcb4804073af9a7cc
cornelk <github@codejuggle.dj>
2022-12-13 20:53:47 -0600

    nes: add more apu constants

--- nes/register/const.go
+++ nes/register/const.go
@@ -3,23 +3,26 @@ package register
 
 // APU (Audio Processing Unit) constants
 const (
-	SQ1_VOL       = 0x4000
-	SQ1_SWEEP     = 0x4001
-	SQ1_LO        = 0x4002
-	SQ1_HI        = 0x4003
-	SQ2_VOL       = 0x4004
-	SQ2_SWEEP     = 0x4005
-	SQ2_LO        = 0x4006
-	SQ2_HI        = 0x4007
-	TRI_LINEAR    = 0x4008
-	TRI_LO        = 0x400A
-	TRI_HI        = 0x400B
-	NOISE_VOL     = 0x400C
-	NOISE_LO      = 0x400E
-	NOISE_HI      = 0x400F
-	APU_DMC_CTRL  = 0x4010
-	APU_CHAN_CTRL = 0x4015
-	APU_FRAME     = 0x4017
+	APU_PL1_VOL    = 0x4000
+	APU_PL1_SWEEP  = 0x4001
+	APU_PL1_LO     = 0x4002
+	APU_PL1_HI     = 0x4003
+	APU_PL2_VOL    = 0x4004
+	APU_PL2_SWEEP  = 0x4005
+	APU_PL2_LO     = 0x4006
+	APU_PL2_HI     = 0x4007
+	APU_TRI_LINEAR = 0x4008
+	APU_TRI_LO     = 0x400A
+	APU_TRI_HI     = 0x400B
+	APU_NOISE_VOL  = 0x400C
+	APU_NOISE_LO   = 0x400E
+	APU_NOISE_HI   = 0x400F
+	APU_DMC_FREQ   = 0x4010
+	APU_DMC_RAW    = 0x4011
+	APU_DMC_START  = 0x4012
+	APU_DMC_LEN    = 0x4013
+	APU_SND_CHN    = 0x4015
+	APU_FRAME      = 0x4017
 )
 
 // Controller constants
