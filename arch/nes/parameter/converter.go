e91fa01544455013f1abbee66b919ed656b9da86
cornelk <github@codejuggle.dj>
2023-08-23 13:33:19 -0600

    parameter: improve converter configurability

--- arch/nes/parameter/converter.go
+++ arch/nes/parameter/converter.go
@@ -9,7 +9,10 @@ import (
 
 // Config contains the configuration for the parameter converter.
 type Config struct {
-	IndirectNoParentheses bool // do not output parentheses for indirect access
+	ZeroPagePrefix string
+	AbsolutePrefix string
+	IndirectPrefix string
+	IndirectSuffix string
 }
 
 // Converter converts the opcode parameters to specific assembler compatible output.
@@ -36,74 +39,114 @@ func (c Converter) Accumulator() string {
 
 // Absolute converts the parameters to the assembler implementation compatible string.
 func (c Converter) Absolute(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.AbsolutePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute:
-		return fmt.Sprintf("$%04X", val)
+		builder.WriteString(fmt.Sprintf("$%04X", val))
 	case string:
-		return val
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	s := builder.String()
+	return s
 }
 
 // AbsoluteX converts the parameters to the assembler implementation compatible string.
 func (c Converter) AbsoluteX(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.AbsolutePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute, addressing.AbsoluteX:
-		return fmt.Sprintf("$%04X,X", val)
+		builder.WriteString(fmt.Sprintf("$%04X", val))
 	case string:
-		return fmt.Sprintf("%s,X", val)
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	builder.WriteString(",X")
+	s := builder.String()
+	return s
 }
 
 // AbsoluteY converts the parameters to the assembler implementation compatible string.
 func (c Converter) AbsoluteY(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.AbsolutePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute, addressing.AbsoluteY:
-		return fmt.Sprintf("$%04X,Y", val)
+		builder.WriteString(fmt.Sprintf("$%04X", val))
 	case string:
-		return fmt.Sprintf("%s,Y", val)
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	builder.WriteString(",Y")
+	s := builder.String()
+	return s
 }
 
 // ZeroPage converts the parameters to the assembler implementation compatible string.
 func (c Converter) ZeroPage(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.ZeroPagePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute, addressing.ZeroPage:
-		return fmt.Sprintf("$%02X", val)
+		builder.WriteString(fmt.Sprintf("$%02X", val))
 	case string:
-		return val
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	s := builder.String()
+	return s
 }
 
 // ZeroPageX converts the parameters to the assembler implementation compatible string.
 func (c Converter) ZeroPageX(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.ZeroPagePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute, addressing.ZeroPage, addressing.ZeroPageX:
-		return fmt.Sprintf("$%02X,X", val)
+		builder.WriteString(fmt.Sprintf("$%02X", val))
 	case string:
-		return fmt.Sprintf("%s,X", val)
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	builder.WriteString(",X")
+	s := builder.String()
+	return s
 }
 
 // ZeroPageY converts the parameters to the assembler implementation compatible string.
 func (c Converter) ZeroPageY(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.ZeroPagePrefix)
+
 	switch val := param.(type) {
 	case int, addressing.Absolute, addressing.ZeroPage, addressing.ZeroPageY:
-		return fmt.Sprintf("$%02X,Y", val)
+		builder.WriteString(fmt.Sprintf("$%02X", val))
 	case string:
-		return fmt.Sprintf("%s,Y", val)
+		builder.WriteString(val)
 	default:
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
+
+	builder.WriteString(",Y")
+	s := builder.String()
+	return s
 }
 
 // Relative converts the parameters to the assembler implementation compatible string.
@@ -116,20 +159,26 @@ func (c Converter) Relative(param any) string {
 
 // Indirect converts the parameters to the assembler implementation compatible string.
 func (c Converter) Indirect(param any) string {
+	var builder strings.Builder
+	builder.WriteString(c.cfg.IndirectPrefix)
+
 	address, ok := param.(addressing.Indirect)
 	if ok {
-		return fmt.Sprintf("($%04X)", address)
+		builder.WriteString(fmt.Sprintf("$%04X", address))
+	} else {
+		alias := param.(string)
+		builder.WriteString(alias)
 	}
-	alias := param.(string)
-	return fmt.Sprintf("(%s)", alias)
+
+	builder.WriteString(c.cfg.IndirectSuffix)
+	s := builder.String()
+	return s
 }
 
 // IndirectX converts the parameters to the assembler implementation compatible string.
 func (c Converter) IndirectX(param any) string {
 	var builder strings.Builder
-	if !c.cfg.IndirectNoParentheses {
-		builder.WriteRune('(')
-	}
+	builder.WriteString(c.cfg.IndirectPrefix)
 
 	switch val := param.(type) {
 	case addressing.Indirect, addressing.IndirectX:
@@ -141,9 +190,7 @@ func (c Converter) IndirectX(param any) string {
 	}
 
 	builder.WriteString(",X")
-	if !c.cfg.IndirectNoParentheses {
-		builder.WriteRune(')')
-	}
+	builder.WriteString(c.cfg.IndirectSuffix)
 	s := builder.String()
 	return s
 }
@@ -151,9 +198,7 @@ func (c Converter) IndirectX(param any) string {
 // IndirectY converts the parameters to the assembler implementation compatible string.
 func (c Converter) IndirectY(param any) string {
 	var builder strings.Builder
-	if !c.cfg.IndirectNoParentheses {
-		builder.WriteRune('(')
-	}
+	builder.WriteString(c.cfg.IndirectPrefix)
 
 	switch val := param.(type) {
 	case addressing.Indirect, addressing.IndirectY:
@@ -164,9 +209,7 @@ func (c Converter) IndirectY(param any) string {
 		panic(fmt.Sprintf("unsupported param type %T", val))
 	}
 
-	if !c.cfg.IndirectNoParentheses {
-		builder.WriteRune(')')
-	}
+	builder.WriteString(c.cfg.IndirectSuffix)
 	builder.WriteString(",Y")
 	s := builder.String()
 	return s
--- arch/nes/parameter/converter_test.go
+++ arch/nes/parameter/converter_test.go
@@ -7,26 +7,54 @@ import (
 	"github.com/retroenv/retrogolib/assert"
 )
 
-func TestParameterDefault(t *testing.T) {
-	cfg := Config{}
-	conv := New(cfg)
-
-	s := conv.IndirectX(addressing.Indirect(0x1000))
-	assert.Equal(t, "($1000,X)", s)
-
-	s = conv.IndirectY(addressing.Indirect(0x1000))
-	assert.Equal(t, "($1000),Y", s)
-}
-
-func TestParameterIndirectNoParentheses(t *testing.T) {
+func TestParameterAbsolute(t *testing.T) {
 	cfg := Config{
-		IndirectNoParentheses: true,
+		AbsolutePrefix: "a:",
 	}
 	conv := New(cfg)
+	var s string
 
-	s := conv.IndirectX(addressing.Indirect(0x1000))
-	assert.Equal(t, "$1000,X", s)
+	s = conv.Absolute(addressing.Absolute(0x1000))
+	assert.Equal(t, "a:$1000", s)
+
+	s = conv.AbsoluteX(addressing.Absolute(0x1000))
+	assert.Equal(t, "a:$1000,X", s)
+
+	s = conv.AbsoluteY(addressing.Absolute(0x1000))
+	assert.Equal(t, "a:$1000,Y", s)
+}
+
+func TestParameterZeroPage(t *testing.T) {
+	cfg := Config{
+		ZeroPagePrefix: "<",
+	}
+	conv := New(cfg)
+	var s string
+
+	s = conv.ZeroPage(addressing.ZeroPage(0x10))
+	assert.Equal(t, "<$10", s)
+
+	s = conv.ZeroPageX(addressing.ZeroPage(0x10))
+	assert.Equal(t, "<$10,X", s)
+
+	s = conv.ZeroPageY(addressing.ZeroPage(0x10))
+	assert.Equal(t, "<$10,Y", s)
+}
+
+func TestParameterIndirect(t *testing.T) {
+	cfg := Config{
+		IndirectPrefix: "[",
+		IndirectSuffix: "]",
+	}
+	conv := New(cfg)
+	var s string
+
+	s = conv.Indirect(addressing.Indirect(0x1000))
+	assert.Equal(t, "[$1000]", s)
+
+	s = conv.IndirectX(addressing.Indirect(0x1000))
+	assert.Equal(t, "[$1000,X]", s)
 
 	s = conv.IndirectY(addressing.Indirect(0x1000))
-	assert.Equal(t, "$1000,Y", s)
+	assert.Equal(t, "[$1000],Y", s)
 }
