9f0291122a101122e44237776acdb32a0cafafef
cornelk <github@codejuggle.dj>
2024-12-18 13:35:45 -0600

    fixes

--- arch/cpu/chip8/emulation_test.go
+++ arch/cpu/chip8/emulation_test.go
@@ -1,1 +1,18 @@
 package chip8
+
+import (
+	"testing"
+
+	"github.com/retroenv/retrogolib/assert"
+)
+
+func TestCls(t *testing.T) {
+	c := New()
+	c.Display[0] = true
+	c.Display[displayWidth+1] = true
+	assert.NoError(t, cls(c, 0))
+
+	for i := 0; i < 64; i++ {
+		assert.False(t, c.Display[i], "Display[%d] is not false", i)
+	}
+}
